8ce18e35e2720e3b7b4826de16c52dd78142e38d
edusantsouza <eduardo.santsouza@gmail.com>
2023-03-28 04:10:02 -0300

    Correções na Home e Slide

--- src/slider-script.js
+++ src/slider-script.js
@@ -3,42 +3,67 @@
   const arrowRight = document.querySelector(".arrow-right");
   const inputs = document.querySelectorAll(".banner-section input");
 
-  const sliderTimer = () => {
-    let number = 0;
-    let seconds = 4000;
-    const coreFunc = () => {
-      if (number < 2) {
-        number++;
-      } else {
-        number = 0;
-      }
-      inputs[number].checked = true;
-    };
+  const image = document.querySelector(".banner-image");
 
-    let interval = setInterval(coreFunc, seconds);
-
-    arrowLeft.addEventListener("click", () => {
-      if (number >= 1) {
-        number -= 1;
-        inputs[number].checked = true;
-      } else if (number === 0) {
-        number += 2;
-        inputs[number].checked = true;
-      }
-      clearInterval(interval);
-    });
-
-    arrowRight.addEventListener("click", () => {
-      if (number <= 1) {
-        number += 1;
-        inputs[number].checked = true;
-      } else if (number === 2) {
-        number -= 2;
-        inputs[number].checked = true;
-      }
-      clearInterval(interval);
-    });
+  let number = 1;
+  let seconds = 4000;
+  const coreFunc = () => {
+    if (number < 3) {
+      number++;
+    } else {
+      number = 1;
+    }
+    image.setAttribute(
+      "style",
+      `background: url('/img/banners/banner-${number}.jpg') no-repeat center center; background-size: auto;`,
+    );
   };
 
-  sliderTimer();
+  setInterval(coreFunc, seconds);
+
+  const clearAllIntervals = () => {
+    for (i = 0; i < 20; i++) {
+      window.clearInterval(i);
+    }
+  };
+
+  arrowLeft.addEventListener("click", () => {
+    if (number === 1) {
+      number += 2;
+      image.setAttribute(
+        "style",
+        `background: url('/img/banners/banner-${number}.jpg') no-repeat center center; background-size: auto;`,
+      );
+    } else if (number >= 2 || number <= 3) {
+      number -= 1;
+      image.setAttribute(
+        "style",
+        `background: url('/img/banners/banner-${number}.jpg') no-repeat center center; background-size: auto;`,
+      );
+    }
+    clearAllIntervals();
+    setTimeout(() => {
+      setInterval(coreFunc, seconds);
+    }, 5000);
+  });
+
+  arrowRight.addEventListener("click", () => {
+    if (number === 1 || number === 2) {
+      number += 1;
+      image.setAttribute(
+        "style",
+        `background: url('/img/banners/banner-${number}.jpg') no-repeat center center; background-size: auto;`,
+      );
+    } else if (number === 3) {
+      number -= 2;
+      image.setAttribute(
+        "style",
+        `background: url('/img/banners/banner-${number}.jpg') no-repeat center center; background-size: auto;`,
+      );
+    }
+    clearAllIntervals();
+    setTimeout(() => {
+      setInterval(coreFunc, seconds);
+    }, 5000);
+  });
 })();
