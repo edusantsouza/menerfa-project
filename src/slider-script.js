5013421923d195980dba51f48e684d502505c0f3
edusantsouza <eduardo.santsouza@gmail.com>
2023-03-30 16:25:24 -0300

    Landing principal finalizada

--- src/slider-script.js
+++ src/slider-script.js
@@ -71,24 +71,39 @@
 
   //Scroll Fixed Header
   const fixedHeader = () => {
-    const header = document.querySelector("header");
     const headerWrapper = document.querySelector(".header-wrapper");
     const headerContent = document.querySelector(".header-content");
     const logoMobile = document.querySelector(".logo-mnf");
+    const verifyScreenPosition = () => {
+      return window.scrollY;
+    };
+    let position;
+    setInterval(() => {
+      position = verifyScreenPosition();
+    }, 500);
 
     window.addEventListener("scroll", () => {
       if (window.scrollY > 10) {
-        headerWrapper.style =
-          "position: fixed; width: 100%; z-index: 150; top: 0; box-shadow: -1px 5px 10px rgba(0, 0, 0, 0.3);";
+        headerWrapper.style = `position: fixed; width: 100%; z-index: 150; top: 0; box-shadow: -1px 5px 10px rgba(0, 0, 0, 0.3); opacity: ${0}; visibility: hidden`;
+
         if (window.outerWidth < 740) {
           logoMobile.style = "display: none ;";
           headerContent.style = "padding: 16px 10px";
         } else {
           logoMobile.style = "display: block ;";
         }
-      } else if (window.scrollY < 100) {
+      } else {
         logoMobile.style = "display: block ;";
         headerWrapper.style = "box-shadow: none;";
+      }
+
+      if (verifyScreenPosition() < position) {
+        headerWrapper.style = `position: fixed; width: 100%; z-index: 150; top: 0; box-shadow: -1px 5px 10px rgba(0, 0, 0, 0.3); opacity: ${1}; visibility: visible`;
+      }
+
+      if (window.scrollY < 5) {
+        logoMobile.style = "display: block ;";
+        headerWrapper.style = "box-shadow: none; position: static";
       }
     });
   };
@@ -117,8 +132,44 @@
     closeBtn.addEventListener("click", closeMenu);
   };
 
+  const activeSearchMobile = () => {
+    const searchBtn = document.querySelector(".search-icon svg");
+    const searchInput = document.querySelector(
+      ".header-content .flex-header .search .search-bar input",
+    );
+    const goSearch = document.querySelector(".opened-search");
+    const goClose = document.querySelector(".close-search");
+    window.addEventListener("resize", () => {
+      if (window.outerWidth > 736) {
+        goSearch.style = "display: none";
+        goClose.style = "display: none";
+        searchInput.style = "width: 0%; padding: 1rem;";
+      } else if (window.outerWidth <= 736) {
+        searchInput.style = "width: 0%; padding: 0rem;";
+        searchBtn.style = " width: 3rem; height: 3rem;";
+        goSearch.style = "display: none";
+        goClose.style = "display: none";
+      }
+    });
+
+    goClose.addEventListener("click", () => {
+      goSearch.style = "display: none";
+      goClose.style = "display: none";
+      searchBtn.style = " width: 3rem; height: 3rem;";
+      searchInput.style = "width: 0%; padding: 0rem;";
+    });
+
+    searchBtn.addEventListener("click", () => {
+      searchBtn.style = "width: 0;";
+      searchInput.style = "width: 100%; padding: 1rem;";
+      goSearch.style = "display: block";
+      goClose.style = "display: block";
+    });
+  };
+
   //Call Functions
   Menu();
+  activeSearchMobile();
   sliderFunc();
   fixedHeader();
 })();
